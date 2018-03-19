87aaedd3b56c2c12c07f98a5503c8aed376d1d63
GForceV8 <37455675+GForceV8@users.noreply.github.com>
2018-03-19 23:23:58 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
-    bot.user.setGame("!aide | MultiGaming");
+    bot.user.setGame("| MultiGaming |");
     console.log("Le bot a bien ete connecte")
 });
 
