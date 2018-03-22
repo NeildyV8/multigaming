4b026f57da3b3455a6b01e54c6457a12eca0308f
GForceV8 <37455675+GForceV8@users.noreply.github.com>
2018-03-22 12:37:18 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -175,13 +175,30 @@ bot.on("message", async function(message) {
 
                 break;
 		    
-		    case "aide2":
+            case "aide2":
             var embed = new Discord.RichEmbed()
                 .addField(":tv: !gforce", "Cette commande vous affiche le lien de la chaîne de live Twitch de GForceV8 !.")
                 .setColor("#0280FD")
                 .setFooter("MultiGaming par @GForceV8")
                 .setAuthor(message.author.username, message.author.avatarURL)
                 .setDescription("Voici la liste des commandes du bot MultiGaming - @GForceV8.")
+                .setTimestamp()
+                message.delete()
+                message.channel.sendEmbed(embed)
+
+                break;
+
+            case "aide3":
+            var embed = new Discord.RichEmbed()
+                .addField(":video_game: ArmA 3","Donne l'accès au channel du jeu.")
+                .addField(":video_game: ETS 2","Donne l'accès au channel du jeu.")
+                .addField(":video_game: FORTNITE","Donne l'accès au channel du jeu.")
+                .addField(":video_game: PALADINS","Donne l'accès au channel du jeu.")
+                .addField(":video_game: "A venir si demande")
+                .setColor("#0280FD")
+                .setFooter("Aide - MultiGaming par @GForceV8")
+                .setAuthor(message.author.username, message.author.avatarURL)
+                .setDescription("Voici la liste des Rôles MultiGaming - par @GForceV8.")
                 .setTimestamp()
                 message.delete()
                 message.channel.sendEmbed(embed)
