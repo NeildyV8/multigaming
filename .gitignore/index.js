a50c56075324f6bab3979b5bfd59a2ea183f127b
GForceV8 <37455675+GForceV8@users.noreply.github.com>
2018-03-22 13:30:17 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -190,11 +190,11 @@ bot.on("message", async function(message) {
  
             case "roles":
             var embed = new Discord.RichEmbed()
-                .addField(":video_game: ArmA 3","Donne l'accès au channel du jeu.")
-                .addField(":video_game: ETS 2","Donne l'accès au channel du jeu.")
-                .addField(":video_game: FORTNITE","Donne l'accès au channel du jeu.")
-                .addField(":video_game: PALADINS","Donne l'accès au channel du jeu.")
-                .addField(":video_game: Bientôt","si demande de jeu.")
+                .addField(":video_game: !arma3","Donne l'accès au channel du jeu.")
+                .addField(":video_game: !ets2","Donne l'accès au channel du jeu.")
+                .addField(":video_game: !fortnite","Donne l'accès au channel du jeu.")
+                .addField(":video_game: !paladins","Donne l'accès au channel du jeu.")
+                .addField(":video_game: !bientôt","si demande de jeu.")
                 .setColor("#0280FD")
                 .setFooter("MultiGaming par @GForceV8")
                 .setAuthor(message.author.username, message.author.avatarURL)
