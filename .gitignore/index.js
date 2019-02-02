069fb200db6536b091544342d48ddfd85c0692a4
GForceV8 <37455675+GForceV8@users.noreply.github.com>
2019-02-02 19:43:14 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
-    bot.user.setGame("| MultiGaming |");
+    bot.user.setGame("| Dev By GForceV8 |");
     console.log("Le bot a bien ete connecte")
 });
 
