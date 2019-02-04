d9fe10bbfedaf84bb141fc4ccfcc99ab69a5d112
GForceV8 <37455675+GForceV8@users.noreply.github.com>
2019-02-04 05:16:05 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
-    bot.user.setGame("| Dev By GForceV8 |");
+    bot.user.setGame("| Dev By [V8] GForce |");
     console.log("Le bot a bien ete connecte")
 });
 
